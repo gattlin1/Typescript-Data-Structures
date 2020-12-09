1ee5332abaf3a149de33eb7c3d8d2906955e1aad
gattlin1 <gattlin1@live.missouristate.edu>
2020-12-08 21:41:20 -0500

    Initial Commit

---
+++ hashmap.ts
@@ -0,0 +1,23 @@
+export class HashMap<K, V> {
+    items: [[K, V]];
+    public count: number;
+
+    public insert(key: K, value: V) {
+
+    }
+
+    public get(key: K): V {
+    }
+
+    public remove(key: K) {
+
+    }
+
+    private resize() {
+
+    }
+
+    private isSparse(): boolean {
+        return true;
+    }
+}
